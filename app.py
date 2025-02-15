2e1ed4c4745d147507927fea148cdc154b5f978a
zahid26 <zahidalaie007@gmail.com>
2025-02-15 21:00:36 +0530

    docker ci/cd yml file

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-## This project shows how to work with Github Actions.
+from flask import Flask
+
+app=Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello World"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+    
---
+++ dockerfile
@@ -0,0 +1,7 @@
+FROM python:3.9-slim
+
+WORKDIR /app
+COPY . /app
+RUN pip install Flask
+EXPOSE 5000
+CMD ["python3", "app.py"]
